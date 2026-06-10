1171571b587a107d6eb96bb64d302c0eb62d3c31
aadithyask99-boop <aadithyask99@gmail.com>
2026-06-10 10:13:54 +0100

    Delete api/BingSiteAuth.js

--- api/BingSiteAuth.js
+++
@@ -1,8 +0,0 @@
-// Serves Bing Webmaster verification XML file
-module.exports = function handler(req, res) {
-  res.setHeader('Content-Type', 'text/xml');
-  res.status(200).send(`<?xml version="1.0"?>
-<users>
-  <user>148DCC9206B1EAB68990C712CBC90D1D</user>
-</users>`);
-};
